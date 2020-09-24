b5064a7d178a4ea3684c8d607317c091d0a1e4ee
G2048 <Borntokill.ix@gmail.com>
2020-09-24 23:35:59 +0700

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,40 +4,33 @@
  * @param {Number} interval
  * @returns {String}
  */
-module.exports = function (hours_input, min_input, interval) {
-	var hours;
-	var minutes_1 = minutes+interval;
-
+module.exports = function (hours, minutes, interval) {
 	
-//	hours_input = min_input/60 % 24
-//	minutes = min_input % 60
-//	print(int(hours), int(minutes))
-//print (int (A * N + (B * N / 100)), B * N % 100)
+	//	hours_input = min_input/60 % 24
+	//	minutes = min_input % 60
+	//	print(int(hours), int(minutes))
+	//  print (int (A * N + (B * N / 100)), B * N % 100)
 
+	hours = Math.floor ((hours + (minutes+interval) / 60)%24);
+	minutes = (minutes + interval) % 60;
 
-	if (0 <= minutes_1 && minutes_1<=59) {
-		minutes= minutes + interval
-	}
-	if (minutes_1==60) {
-			minutes= '00';
-			hours = hours+1;
-		}
-    if (minutes_1>60) {
-    	minutes= minutes+ (minutes_1%60);
-    	hours = Math.floor (minutes_1/60) + hours
+	if (hours < 10) {
+		hours= '0' + String(hours)
+	};
 
-    }
+	if (minutes < 10) {
+		minutes = '0' + String(minutes)
+	};
 
-	if (0<= hours && hours <=23 && minutes_1!=60) {
-		hours= hours + Math.floor (minutes_1/60)
-	}
-	if (hours==24) {
+	if (minutes == 0 || minutes == 60) {
+		minutes = '00';
+	};
+
+	if (hours == 0 || hours % 24 == 0) {
 		hours = '00';
-	}		
-	
+	};
 
 	var result= String (hours)+':'+ String (minutes);
-	console.info (result)
 
 	return result;
 };
